479eb6fd5b05379abb032ef8a166f122467d70fe
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-03-11 22:43:16 +0100

    Remove some periods from lang strings

--- server/resources/lang/en/admin/boats.php
+++ server/resources/lang/en/admin/boats.php
@@ -23,13 +23,13 @@ return [
     // Admin boats show page
     'show.title' => ':boat.name - Boats - Admin',
     'show.owner' => 'Owner:',
-    'show.description_empty' => 'This boat has no description.',
+    'show.description_empty' => 'This boat has no description',
     'show.edit' => 'Edit boat',
     'show.delete' => 'Delete boat',
 
     'show.boat_types' => 'Boat Types',
     'show.boat_types_remove_button' => 'Remove boat type',
-    'show.boat_types_empty' => 'This boat has no assigned boat types.',
+    'show.boat_types_empty' => 'This boat has no assigned boat types',
     'show.boat_types_field' => 'Select a boat type...',
     'show.boat_types_add_button' => 'Add boat type',
 
@@ -37,7 +37,7 @@ return [
     'show.users_role_crew' => 'CREW',
     'show.users_role_captain' => 'CAPTAIN',
     'show.users_remove_button' => 'Remove user',
-    'show.users_empty' => 'This boat has no assigned users.',
+    'show.users_empty' => 'This boat has no assigned users',
     'show.users_field' => 'Select a user...',
     'show.users_role_field_crew' => 'As crew',
     'show.users_role_field_captain' => 'As captain',
