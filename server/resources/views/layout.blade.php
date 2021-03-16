8fcc958141e2fe29168cd4703f8cdb6e3d4888da
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-03-16 15:51:50 +0100

    Updated some colors and svg metadata

--- server/resources/views/layout.blade.php
+++ server/resources/views/layout.blade.php
@@ -10,8 +10,8 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/images/icon-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/images/icon-32x32.png">
     <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
-    <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#1166bb">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#276cda">
+    <meta name="theme-color" content="#f5f5f5">
     <link rel="manifest" href="/manifest.json">
     <link rel="stylesheet" href="/css/bulma.min.css">
 </head>
