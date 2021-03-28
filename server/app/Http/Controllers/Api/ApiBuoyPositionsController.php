71410b9c127ea88e93e4209cb23b8d41912f1a74
Stan Boer <stanboer@gmail.com>
2021-03-28 19:49:56 +0200

    Added API for buoys and positions

---
+++ server/app/Http/Controllers/Api/ApiBuoyPositionsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Buoy;
+use App\Rules\Latitude;
+use App\Rules\Longitude;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ApiBuoyPositionsController extends Controller
+{
+    // API buoy positions index route
+    public function index(Buoy $buoy)
+    {
+        // Return the boat positions
+        return $buoy->positions->paginate(config('pagination.api.limit'));
+    }
+
+    // API buoy positions store route
+    public function store(Request $request, Buoy $buoy)
+    {
+        // Validate input
+        $validation = Validator::make($request->all(), [
+            'latitude' => ['required', new Latitude],
+            'longitude' => ['required', new Longitude]
+        ]);
+        if ($validation->fails()) {
+            return response([ 'errors' => $validation->errors() ], 400);
+        }
+
+        // Create buoy position
+        $buoyPosition = $buoy->positions()->create([
+            'latitude' => request('latitude'),
+            'longitude' => request('longitude')
+        ]);
+
+        // Return the new created boat position
+        return $buoyPosition;
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -5,19 +5,23 @@ use App\Http\Controllers\Api\ApiBoatPositionsController;
 use App\Http\Controllers\Api\ApiBoatBoatTypesController;
 use App\Http\Controllers\Api\ApiBoatUsersController;
 
+use App\Http\Controllers\Api\ApiBuoysController;
+use App\Http\Controllers\Api\ApiBuoyPositionsController;
+
 use Illuminate\Support\Facades\Route;
 
 // TODO: API auth
 
-// Api boat routes
+// API boat routes
 Route::get('boats', [ApiBoatsController::class, 'index']);
-
 Route::get('boats/{boat}', [ApiBoatsController::class, 'show']);
-
 Route::get('boats/{boat}/positions', [ApiBoatPositionsController::class, 'index']);
-
 Route::post('boats/{boat}/positions', [ApiBoatPositionsController::class, 'store']);
-
 Route::get('boats/{boat}/boat_types', [ApiBoatBoatTypesController::class, 'index']);
-
 Route::get('boats/{boat}/users', [ApiBoatUsersController::class, 'index']);
+
+// API buoy routes
+Route::get('buoys', [ApiBuoysController::class, 'index']);
+Route::get('buoys/{buoy}', [ApiBuoysController::class, 'show']);
+Route::get('buoys/{buoy}/positions', [ApiBuoyPositionsController::class, 'index']);
+Route::post('buoys/{buoy}/positions', [ApiBuoyPositionsController::class, 'store']);
