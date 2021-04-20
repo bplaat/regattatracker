35edc97f37a1b33165f2d2131230cbc8a08688fd
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-20 15:48:43 +0200

    Created competition class infrastructure.

---
+++ server/database/factories/CompetitionClassFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CompetitionClass;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompetitionClassFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = CompetitionClass::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
