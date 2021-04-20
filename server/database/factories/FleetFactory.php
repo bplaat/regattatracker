0cebab0f3f204c4d904c2722b6b7c513099fda7e
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-20 15:50:15 +0200

    Created fleet infrastructure.

---
+++ server/database/factories/FleetFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Fleet;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FleetFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Fleet::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
