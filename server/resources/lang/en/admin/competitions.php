92286b8bcf93f01823847ce11700eb98c7672b59
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-22 12:02:45 +0200

    Added competition index lang entries, added competitions to admin home.

---
+++ server/resources/lang/en/admin/competitions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Admin competitions index page
+    'index.title' => 'Competitions - Admin',
+    'index.breadcrumb' => 'Competitions',
+    'index.header' => 'All competitions',
+    'index.search_field' => 'Search for competitions...',
+    'index.search_button' => 'Search',
+    'index.empty' => 'No competitions found',
+    'index.create' => 'Create new competition'
+];
