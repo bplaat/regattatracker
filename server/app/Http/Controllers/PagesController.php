8b9e9c692b8d34e6cc76df9b3235326b498ac139
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-04-28 08:50:46 +0200

    Fix a bug with empty boats and buoys on live map

--- server/app/Http/Controllers/PagesController.php
+++ server/app/Http/Controllers/PagesController.php
@@ -15,27 +15,33 @@ class PagesController extends Controller
 
         // Get all boats and there positions of today
         $boats = Boat::all();
+        $goodBoats = collect();
         foreach ($boats as $boat) {
             $boat->positions = $boat->positionsByDay(time());
+
             if ($boat->positions->count() > 0) {
                 $itemsData = true;
+                $goodBoats->push($boat);
             }
         }
 
         // Get all boats and there positions of today
         $buoys = Buoy::all();
+        $goodBuoys = collect();
         foreach ($buoys as $buoy) {
             $buoy->positions = $buoy->positionsByDay(time());
+
             if ($buoy->positions->count() > 0) {
                 $itemsData = true;
+                $goodBuoys->push($buoy);
             }
         }
 
         // Return the home route
         return view('home', [
             'itemsData' => $itemsData,
-            'boats' => $boats,
-            'buoys' => $buoys
+            'boats' => $goodBoats,
+            'buoys' => $goodBuoys
         ]);
     }
 }
