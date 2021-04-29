bca7e952c498b7dc8376b032f9827f86948ae2d5
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-29 17:07:32 +0200

    Added admin finish edit page skeleton.

---
+++ server/resources/lang/en/admin/finishes.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'edit.point_a' => 'Point A',
+    'edit.point_b' => 'Point B',
+    'edit.latitude_a_field' => 'Latitude A',
+    'edit.latitude_b_field' => 'Latitude B',
+    'edit.longitude_a_field' => 'Longitude A',
+    'edit.longitude_b_field' => 'Longitude B',
+    'edit.button' => 'Create finish',
+];
