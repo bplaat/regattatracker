a3e7d61220acee43278ff90214533dd257175b6c
IpsumCapra <samuelcornelisse@gmail.com>
2021-05-16 19:52:37 +0200

    Setup eventclass and fleet controllers.

--- server/app/Http/Controllers/Admin/AdminEventClassesController.php
+++ server/app/Http/Controllers/Admin/AdminEventClassesController.php
@@ -9,5 +9,42 @@ use Illuminate\Http\Request;
 
 class AdminEventClassesController extends Controller
 {
+    // Admin event class store route
+    public function store(Request $request, Event $event) {
+        $fields = $request->validate([
+            'name' => 'required|min:2|max:255'
+        ]);
 
+        Event::create([
+           'name' => $fields['name'],
+           'event_id' => $event->id
+        ]);
+
+        return redirect()->route('admin.events.show', ['event' => $event]);
+    }
+
+    // Admin event class update route
+    public function update(Request $request, Event $event, EventClass $eventClass) {
+        $fields = $request->validate([
+            'name' => 'required|min:2|max:255'
+        ]);
+
+        $eventClass->update([
+           'name' => $fields['name']
+        ]);
+
+        return redirect()->route('admin.events.show', ['event' => $event]);
+    }
+
+    // Admin event class edit route
+    public function edit(Event $event, EventClass $eventClass) {
+        return view('admin.events.classes.edit', ['event' => $event, 'class' => $eventClass]);
+    }
+
+    // Admin event class delete route
+    public function delete(Event $event, EventClass $eventClass) {
+        $eventClass->delete();
+
+        return redirect()->route('admin.events.show', ['event' => $event]);
+    }
 }
--- server/app/Http/Controllers/Admin/AdminFleetsController.php
+++ server/app/Http/Controllers/Admin/AdminFleetsController.php
@@ -3,11 +3,49 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use App\Models\EventClass;
 use App\Models\Fleet;
 use Illuminate\Http\Request;
 
 class AdminFleetsController extends Controller
 {
+    // Admin fleet store route
+    public function store(Request $request, Event $event, EventClass $eventClass) {
+        $fields = $request->validate([
+            'name' => 'required|min:2|max:255'
+        ]);
 
+        Event::create([
+            'name' => $fields['name'],
+            'event_class_id' => $eventClass->id
+        ]);
+
+        return redirect()->route('admin.events.show', ['event' => $event]);
+    }
+
+    // Admin fleet update route
+    public function update(Request $request, Event $event, Fleet $fleet) {
+        $fields = $request->validate([
+            'name' => 'required|min:2|max:255'
+        ]);
+
+        $fleet->update([
+            'name' => $fields['name']
+        ]);
+
+        return redirect()->route('admin.events.show', ['event' => $event]);
+    }
+
+    // Admin fleet edit route
+    public function edit(Event $event, EventClass $eventClass, Fleet $fleet) {
+        return view('admin.events.classes.fleets.edit', ['event' => $event, 'class' => $eventClass, 'fleet' => $fleet]);
+    }
+
+    // Admin fleet delete route
+    public function delete(Event $event, Fleet $fleet) {
+        $fleet->delete();
+
+        return redirect()->route('admin.events.show', ['event' => $event]);
+    }
 }
