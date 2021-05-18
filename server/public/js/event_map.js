ba67830db1a1a4b55c9fbd9e3d151a5cc0da131d
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-05-18 12:34:08 +0200

    Fixed a small bug with empty paths

--- server/public/js/event_map.js
+++ server/public/js/event_map.js
@@ -26,13 +26,13 @@ class EditorButtonsControl {
                 </svg>
             </button>
 
+            <button type="button"></button>
+
             <button type="button" title="${strings.add_finish_button}">
                 <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M17,14H19V17H22V19H19V22H17V19H14V17H17V14M12.4,5H18V12C15.78,12 13.84,13.21 12.8,15H11L10.6,13H5V20H3V3H12L12.4,5Z" />
                 </svg>
             </button>
-
-            <button type="button"></button>
 
             <button type="button" title="${strings.save_button}">
                 <svg style="width:24px;height:24px" viewBox="0 0 24 24">
@@ -43,8 +43,8 @@ class EditorButtonsControl {
         this.updateItems();
 
         this._container.children[0].addEventListener('click', this.addPoint.bind(this));
-        this._container.children[1].addEventListener('click', this.addFinish.bind(this));
-        this._container.children[2].addEventListener('click', this.toggleConnected.bind(this));
+        this._container.children[1].addEventListener('click', this.toggleConnected.bind(this));
+        this._container.children[2].addEventListener('click', this.addFinish.bind(this));
         this._container.children[3].addEventListener('click', this.save.bind(this));
 
         return this._container;
@@ -52,13 +52,13 @@ class EditorButtonsControl {
 
     updateItems() {
         if (event.connected) {
-            this._container.children[2].title = strings.disconnect_button;
-            this._container.children[2].innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            this._container.children[1].title = strings.disconnect_button;
+            this._container.children[1].innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z" />
             </svg>`;
         } else {
-            this._container.children[2].title = strings.connect_button;
-            this._container.children[2].innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            this._container.children[1].title = strings.connect_button;
+            this._container.children[1].innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M2,5.27L3.28,4L20,20.72L18.73,22L13.9,17.17L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L12.5,15.76L10.88,14.15C10.87,14.39 10.77,14.64 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C8.12,13.77 7.63,12.37 7.72,11L2,5.27M12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.79,8.97L9.38,7.55L12.71,4.22M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.2,10.54 16.61,12.5 16.06,14.23L14.28,12.46C14.23,11.78 13.94,11.11 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z" />
             </svg>`;
         }
@@ -92,14 +92,14 @@ class EditorButtonsControl {
         updateMapItems();
     }
 
-    addFinish() {
-        alert('Todo!');
-    }
-
     toggleConnected() {
         event.connected = !event.connected;
         this.updateItems();
         updateMapItems();
+    }
+
+    addFinish() {
+        alert('Todo!');
     }
 
     save() {
@@ -388,7 +388,7 @@ function updateMapItems() {
         type: 'Feature',
         geometry: {
             type: 'LineString',
-            coordinates: (event.connected ? path.concat([path[0]]) : path).map(point => [point.lng, point.lat])
+            coordinates: path.length > 0 ? (event.connected ? path.concat([path[0]]) : path).map(point => [point.lng, point.lat]) : []
         }
     };
 
