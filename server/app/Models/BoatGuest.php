060597ad9630d67e22ed8ea3116da90df1f53da0
RowdeyGoos <rowdey_goos@hotmail.com>
2021-06-10 17:18:44 +0200

    Added boat guest model

---
+++ server/app/Models/BoatGuest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BoatGuest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'boat_id',
+        'firstname',
+        'insertion',
+        'lastname',
+        'gender',
+        'phone'
+    ];
+
+    // Get user full name (firstname insertion lastname)
+    public function getNameAttribute()
+    {
+        if ($this->insertion != null) {
+            return $this->firstname . ' ' . $this->insertion . ' ' . $this->lastname;
+        } else {
+            return $this->firstname . ' ' . $this->lastname;
+        }
+    }
+
+    // A boat guest belongs to a  boat
+    public function Boat()
+    {
+        return $this->belongsTo(Boat::class);
+    }
+
+}
